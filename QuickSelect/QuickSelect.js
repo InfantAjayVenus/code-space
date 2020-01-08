b6ef0a730634fd18be65e15d0739e578d1eead2a
Infant Ajay Venus <InfantAjay.Venus@cognizant.com>
2020-01-08 18:00:33 +0530

    adding quick select

---
+++ QuickSelect/QuickSelect.js
@@ -0,0 +1,7 @@
+
+const sort = (inputList, k) => {
+    return 
+}
+
+console.log(`sort(2, 6, 3, 9, 1, 5) => ${sort([2, 6, 3, 9, 1, 5])}`);
+console.log(`sort(2, 6, 3, 9, 1) => ${sort([2, 6, 3, 9, 1])}`);
