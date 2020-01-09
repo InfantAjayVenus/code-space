2c448609684e286c28987fc870bdff3c854a474b
Infant Ajay Venus <InfantAjay.Venus@cognizant.com>
2020-01-09 17:54:23 +0530

    adding quick select

--- QuickSelect/QuickSelect.js
+++ QuickSelect/QuickSelect.js
@@ -1,7 +1,28 @@
+const sortedPartition = (inputList, pivotElement) => ({
+    firstList: inputList.filter(listItem => listItem < pivotElement),
+    pivotElement,
+    secondList: inputList.filter(listItem => listItem > pivotElement)
+})
 
-const sort = (inputList, k) => {
-    return 
+const getKthSmallest = (inputList, k) => {
+    
+    if (k < 1 || k > inputList.length) {
+        return Error("Term k must be within range of input list length");
+    }
+    const { firstList, pivotElement, secondList } = sortedPartition(inputList, inputList[0]);
+    
+    if (firstList.length === k - 1) {
+        return pivotElement;
+    } else if (firstList.length >= k) {
+        return getKthSmallest(firstList, k);
+    } else {
+        return getKthSmallest(secondList, k - firstList.length - 1);
+    }
+
 }
 
-console.log(`sort(2, 6, 3, 9, 1, 5) => ${sort([2, 6, 3, 9, 1, 5])}`);
-console.log(`sort(2, 6, 3, 9, 1) => ${sort([2, 6, 3, 9, 1])}`);
+console.log(`1st Smallest(2, 6, 3, 9, 1, 5) => ${getKthSmallest([2, 6, 3, 9, 1, 5], 1)}`);
+console.log(`4th Smallest(2, 6, 3, 9, 1, 5) => ${getKthSmallest([2, 6, 3, 9, 1, 5], 4)}`);
+console.log(`last Smallest(2, 6, 3, 9, 1, 5) => ${getKthSmallest([2, 6, 3, 9, 1, 5], 6)}`);
+console.log(`0th Smallest(2, 6, 3, 9, 1, 5) => ${getKthSmallest([2, 6, 3, 9, 1, 5], 0)}`);
+console.log(`7th Smallest(2, 6, 3, 9, 1, 5) => ${getKthSmallest([2, 6, 3, 9, 1, 5], 7)}`);
---
+++ QuickSelect/QuickSelect.py
@@ -0,0 +1,36 @@
+def sortedInsert(inputList, pivotPosition) :
+    forwardIndex = 0
+    backwardIndex = len(inputList) - 1
+    pivotElement = inputList[pivotPosition]
+    while forwardIndex < backwardIndex :
+        while inputList[forwardIndex] < pivotElement :
+            forwardIndex = forwardIndex + 1
+
+        while inputList[backwardIndex] > pivotElement :
+            backwardIndex = backwardIndex - 1
+
+        if forwardIndex < backwardIndex :
+            inputList[forwardIndex], inputList[backwardIndex] = inputList[backwardIndex], inputList[forwardIndex]
+
+    return forwardIndex
+
+def getKthSmallest(inputList, k):
+    if k < 1 or k > len(inputList):
+        return "Key Term exceeds list range"
+    
+    print("Input List: {}; Key Term: {}".format(inputList, k))
+    pivotPosition = sortedInsert(inputList, 0)
+    print("After Insert: List => {}; PivotPosition => {}".format(inputList, pivotPosition))
+
+    if pivotPosition == k - 1 :
+        return inputList[pivotPosition - 1]
+    elif pivotPosition >= k:
+        return getKthSmallest(inputList[:pivotPosition], k)
+    else:
+        return getKthSmallest(inputList[pivotPosition+1:], k - pivotPosition + 1 )
+
+# print("1st Smallest(2, 6, 3, 9, 1, 5) => {}".format(getKthSmallest([2, 6, 3, 9, 1, 5], 1)))
+print("4th Smallest(2, 6, 3, 9, 1, 5) => {}".format(getKthSmallest([2, 6, 3, 9, 1, 5], 4)))
+# print("last Smallest(2, 6, 3, 9, 1, 5) => {}".format(getKthSmallest([2, 6, 3, 9, 1, 5], 6)))
+# print("0th Smallest(2, 6, 3, 9, 1, 5) => {}".format(getKthSmallest([2, 6, 3, 9, 1, 5], 0)))
+# print("7th Smallest(2, 6, 3, 9, 1, 5) => {}".format(getKthSmallest([2, 6, 3, 9, 1, 5], 7)))
