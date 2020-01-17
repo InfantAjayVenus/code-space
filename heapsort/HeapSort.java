68a219201303b957510d32fd627e4d402778a9ec
Infant Ajay Venus <InfantAjay.Venus@cognizant.com>
2020-01-17 17:48:47 +0530

    adding Heap Sort

--- heapsort/HeapSort.java
+++ heapsort/HeapSort.java
@@ -4,99 +4,49 @@ class HeapSort {
     public int[] sort(int[]inputList) {
         this.list = inputList;
         for(int i = (this.list.length / 2) - 1; i >= 0; i--) {
-            heapifyDownward(0, i);
+            maxHeapify(i, this.list.length);
         }
+
 
         for(int i = this.list.length - 1; i>=0; i--) {
             int temp = this.list[0];
             this.list[0] = this.list[i];
             this.list[i] = temp;
-
-            heapifyUpward(i);
+            maxHeapify(0, i);
         }
         return this.list;
     }
 
-    @SuppressWarnings("unused")
-    private void heapify(int currentNodeIndex) {
-        int smallestIndex = currentNodeIndex;
-        int rightChildIndex = this.getRightChildIndex(currentNodeIndex);
-        int leftChildIndex = this.getLeftChildIndex(currentNodeIndex);
+    private void maxHeapify(int currentNodeIndex, int length) {
+        int largestIndex = currentNodeIndex;
+        int rightChildIndex = HeapSort.getRightChildIndex(currentNodeIndex);
+        int leftChildIndex = HeapSort.getLeftChildIndex(currentNodeIndex);
 
-        if(leftChildIndex < this.list.length && this.list[leftChildIndex] < this.list[smallestIndex]){
-            smallestIndex = leftChildIndex;
+        if(leftChildIndex < length && this.list[leftChildIndex] > this.list[largestIndex]){
+            largestIndex = leftChildIndex;
         }
 
-        if(rightChildIndex < this.list.length && this.list[rightChildIndex] < this.list[smallestIndex]){
-            smallestIndex = rightChildIndex;
+        if(rightChildIndex < length && this.list[rightChildIndex] > this.list[largestIndex]){
+            largestIndex = rightChildIndex;
         }
 
-        if(smallestIndex != currentNodeIndex) {
-            int temp = this.list[smallestIndex];
-            this.list[smallestIndex] = this.list[currentNodeIndex];
+        if(largestIndex != currentNodeIndex) {
+            int temp = this.list[largestIndex];
+            this.list[largestIndex] = this.list[currentNodeIndex];
             this.list[currentNodeIndex] = temp;
 
-            this.heapify(smallestIndex);
-        }
-
-    }
-
-    private void heapifyUpward(int currentNodeIndex) {
-        int smallestIndex = currentNodeIndex;
-        int parentNodeIndex = this.getParentIndex(currentNodeIndex);
-
-        if(currentNodeIndex > 0 && this.list[parentNodeIndex] < this.list[smallestIndex]) {
-            smallestIndex = parentNodeIndex;
-        }
-
-        if(smallestIndex != currentNodeIndex) {
-            int temp = this.list[currentNodeIndex];
-            this.list[currentNodeIndex] = this.list[smallestIndex];
-            this.list[smallestIndex] = temp;
-
-            this.heapifyUpward(smallestIndex);
-        }
-        
-    }
-
-    private void heapifyDownward(int currentNodeIndex, int length) {
-        int smallestIndex = currentNodeIndex;
-        int rightChildIndex = this.getRightChildIndex(currentNodeIndex);
-        int leftChildIndex = this.getLeftChildIndex(currentNodeIndex);
-
-        if(leftChildIndex < length && this.list[leftChildIndex] < this.list[smallestIndex]){
-            smallestIndex = leftChildIndex;
-        }
-
-        if(rightChildIndex < length && this.list[rightChildIndex] < this.list[smallestIndex]){
-            smallestIndex = rightChildIndex;
-        }
-
-        if(smallestIndex != currentNodeIndex) {
-            int temp = this.list[smallestIndex];
-            this.list[smallestIndex] = this.list[currentNodeIndex];
-            this.list[currentNodeIndex] = temp;
-
-            this.heapifyDownward(smallestIndex, length);
+            this.maxHeapify(largestIndex, length);
         }
     }
 
-    @SuppressWarnings("unused")
-    private int getParentIndex(int currentIndex) {
-        return (currentIndex-1)/2;
-    }
-
-    @SuppressWarnings("unused")
-    private int getLeftChildIndex(int currentIndex) {
+    private static int getLeftChildIndex(int currentIndex) {
         return (2*currentIndex)+1;
     }
 
-    @SuppressWarnings("unused")
-    private int getRightChildIndex(int currentIndex) {
+    private static int getRightChildIndex(int currentIndex) {
         return (2*currentIndex)+2;
     }
 
-    @SuppressWarnings("unused")
     public static void printArray(int[] array, String message) {
         System.out.print(message + " => ");
         for (int num : array) {
@@ -105,7 +55,6 @@ class HeapSort {
         System.out.println();
     }
 
-    @SuppressWarnings("unused")
     public static void printArray(int[] array, int rangeStart, int rangeEnd, String message) {
         System.out.print(message + " => ");
         for (int i = rangeStart; i < rangeEnd; i++) {
@@ -114,9 +63,22 @@ class HeapSort {
         System.out.println();
     }
     
+    public static void printHeap(int[] heapArray, String message){
+        System.out.println(message);
+        printHeap(heapArray, 0, "");
+    }
+
+    public static void printHeap(int[] heapArray, int currentNodeIndex, String preceed){
+        if(currentNodeIndex < heapArray.length){
+            System.out.println(preceed+heapArray[currentNodeIndex]);
+            printHeap(heapArray, getLeftChildIndex(currentNodeIndex), preceed+"--");
+            printHeap(heapArray, getRightChildIndex(currentNodeIndex), preceed+"--");
+        }
+    }
+
     public static void main(String[] args) {
         HeapSort sortingObj = new HeapSort();
         HeapSort.printArray(sortingObj.sort(new int[] { 2, 6, 3, 9, 1, 5 }), "sort(2, 6, 3, 9, 1, 5)");
-        // HeapSort.printArray(sortingObj.sort(new int[] { 2, 6, 3, 9, 1 }), "sort(2, 6, 3, 9, 1)");
+        HeapSort.printArray(sortingObj.sort(new int[] { 2, 6, 3, 9, 1 }), "sort(2, 6, 3, 9, 1)");
     }
 }
